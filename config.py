efd66924d2505527ff0e543c5172c4d7be2b0e13
Кириллова Маргарита <115624497+RitaK2@users.noreply.github.com>
2022-11-19 14:56:06 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = "5772117518:AAF0W3WTODCC7gjW_YgOFZAn6wjjxZRggoM"
+
+
+keys ={
+    "биткоин": "BTC",
+    "эфириум": "ETH",
+    "доллар": "USD"
+}
---
+++ extensions.py
@@ -0,0 +1,30 @@
+import requests
+import json
+from config import keys
+
+class ConvertionException(Exception):
+    pass
+
+class CriptoConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionException(f'Нельзя перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Неудалось обработать валюту {quote}.')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Неудалось обработать валюту {base}.')
+
+        try:
+            amount = int(amount)
+        except ValueError:
+            raise ConvertionException(f'Неудалось обработать колличество {amount}.')
+        r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}")
+        total_base = json.loads(r.content)[keys[base]]
+        return total_base * amount
